cc8cd483f02b0c7d956d3dccf95b007853c69548
Nick <167200949+NickFellah@users.noreply.github.com>
2026-01-12 21:37:50 +0000

    Bomd defuse, wasnt included before, it shold be totally working and perfect! righttttt!!

--- gamemodes/homigrad/gamemode/game/levels/bombdefuse/init_tier_0_sh.lua
+++ gamemodes/homigrad/gamemode/game/levels/bombdefuse/init_tier_0_sh.lua
@@ -1,4 +1,4 @@
---table.insert(LevelList,"bombdefuse")
+table.insert(LevelList,"bombdefuse")
 bombdefuse = {}
 bombdefuse.Name = "Bomb Defuse"
 
@@ -6,14 +6,14 @@ bombdefuse.terrorists = {"Terrorists",Color(176,0,0),
 	weapons = {"megamedkit","weapon_binokle","weapon_hands","weapon_hg_hatchet","med_band_small","med_band_big","med_band_small","painkiller","weapon_handcuffs","weapon_radio"},
 	main_weapon = {"weapon_asval", "weapon_mp5", "weapon_m3super"},
 	secondary_weapon = {"weapon_beretta","weapon_p99","weapon_beretta"},
-	models = {"models/player/leet.mdl","models/player/phoenix.mdl"}
+	models = {"models/player/leet.mdl","models/player/phoenix.mdl","models/player/arctic.mdl","models/player/guerilla.mdl"}
 }
 
 bombdefuse.counterterrorists = {"Counter-Terrorists",Color(79,59,187),
 	weapons = {"megamedkit","weapon_binokle","weapon_hg_hatchet","weapon_hands","med_band_big","med_band_small","medkit","painkiller","weapon_handcuffs","weapon_radio"},
 	main_weapon = {"weapon_m4a1","weapon_mp7","weapon_galil"},
 	secondary_weapon = {"weapon_hk_usp", "weapon_mateba"},
-	models = {"models/player/riot.mdl"}
+	models = {"models/player/riot.mdl","models/player/gasmask.mdl","models/player/swat","models/player/urban.mdl"}
 }
 
 bombdefuse.teamEncoder = {
